495555782263d153cdb09e3764d886c641c8da82
Manav Bokinala <manav.bokinala@gmail.com>
2018-07-07 19:17:48 -0700

    After rubocop autocorrect

--- level.rb
+++ level.rb
@@ -3,98 +3,89 @@ require_relative 'tile'
 require_relative 'player'
 
 class Level
-	def initialize(window, player, level_data)
-		@window        = window
-		@tiles         = []
-		@player        = player
-		@level_data    = level_data
-		@total_rows    = 0
-		@total_columns = 0
-		@exit_reached  = false
+  def initialize(window, player, level_data)
+    @window        = window
+    @tiles         = []
+    @player        = player
+    @level_data    = level_data
+    @total_rows    = 0
+    @total_columns = 0
+    @exit_reached  = false
 
-		if @level_data
-			@total_rows = @level_data.length
-			if @total_rows > 0
-				@total_columns = @level_data.first.length
-			end
-			setup_level
-		end
-	end
-
-def setup_level
-  @level_data.each_with_index do |row_data, row|
-    column = 0
-    row_data.each_char do |cell_type|
-      tile = Tile.new(@window, column, row, cell_type)
-      # Change behavior depending on cell_type
-      if tile.is_start?
-        @player.move_to(column, row)
-      end
-      @tiles.push(tile)
-      column += 1
+    if @level_data
+      @total_rows = @level_data.length
+      @total_columns = @level_data.first.length if @total_rows > 0
+      setup_level
     end
   end
-end
 
-	def button_down(id)
-		if level_over?
-			return
-		end
-		column_delta = 0
-		row_delta = 0
+  def setup_level
+    @level_data.each_with_index do |row_data, row|
+      column = 0
+      row_data.each_char do |cell_type|
+        tile = Tile.new(@window, column, row, cell_type)
+        # Change behavior depending on cell_type
+        @player.move_to(column, row) if tile.is_start?
+        @tiles.push(tile)
+        column += 1
+      end
+    end
+  end
 
-		if id == Gosu::KbLeft
-			column_delta = -1
-		elsif id == Gosu::KbRight
-			column_delta = 1
-		end
+  def button_down(id)
+    return if level_over?
+    column_delta = 0
+    row_delta = 0
 
-		if id == Gosu::KbUp
-			row_delta = -1
-		elsif id == Godu::KbDown
-			row_delta = 1
-		end
+    if id == Gosu::KbLeft
+      column_delta = -1
+    elsif id == Gosu::KbRight
+      column_delta = 1
+    end
 
-		if move_valid?(@player, column_delta, row_delta)
-			@player.move_by(column_delta, row_delta)
-			tile = get_tile(@player.column, @player.row)
-			if tile.is_exit?
-				@exit_reached = true
-				tile.hide!
-			else
-				@player.pick_up(tile)
-			end
-		end
-	end
+    if id == Gosu::KbUp
+      row_delta = -1
+    elsif id == Godu::KbDown
+      row_delta = 1
+    end
 
-	def update
-	end
+    if move_valid?(@player, column_delta, row_delta)
+      @player.move_by(column_delta, row_delta)
+      tile = get_tile(@player.column, @player.row)
+      if tile.is_exit?
+        @exit_reached = true
+        tile.hide!
+      else
+        @player.pick_up(tile)
+      end
+    end
+  end
 
-	def draw
-		@tiles.each do |tile|
-			tile.draw
-		end
-		@player.draw
-	end
+  def update; end
 
-	def get_tile(column, row)
-		if column < 0 || column >= @total_columns || row < 0 || row >= @total_rows
-			nil
-		else
-			@tiles[row * @total_columns + column]
-		end
-	end
+  def draw
+    @tiles.each(&:draw)
+    @player.draw
+  end
 
-	def move_valid?(player, column_delta, row_delta)
-		destination = getTile(player.column + column_delta, player_row + row_delta)
-		if destination && destination.tile_can_be_entered?
-			true
-		else
-			false
-		end
-	end
+  def get_tile(column, row)
+    if column < 0 || column >= @total_columns || row < 0 || row >= @total_rows
+      nil
+    else
+      @tiles[row * @total_columns + column]
+    end
+  end
 
-	def level_over?
-		@exit_reached
-	end
+  def move_valid?(player, column_delta, row_delta)
+    destination = getTile(player.column + column_delta, player_row + row_delta)
+    if destination && destination.tile_can_be_entered?
+      true
+    else
+      false
+    end
+  end
+
+  def level_over?
+    @exit_reached
+  end
 end
